11c9fd2ba69215d09e55bcce87422edf1d3cace5
Weronika Przybył <przybyl.weronika@gmail.com>
2024-11-21 15:08:09 +0100

    added new folder with 2 files

---
+++ temp/tryout.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main() {
+    printf("Probna wiadomosc do tryout");
+    return 0;
+}
---
+++ temp/tryout2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main() {
+    printf("tryout 2");
+    return 0;
+}
